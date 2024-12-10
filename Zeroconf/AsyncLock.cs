ab6418f1fe1e6406613a47cf32c88885b760b29d
Martin Løbger <martin@lobger.com>
2024-12-10 16:40:34 +0100

    Do not expose inner classes of AsyncLock

--- Zeroconf/AsyncLock.cs
+++ Zeroconf/AsyncLock.cs
@@ -10,41 +10,43 @@ namespace Zeroconf
     class AsyncLock
 #pragma warning restore CA1001 // Types that own disposable fields should be disposable
     {
-        readonly SemaphoreSlim m_semaphore;
-        readonly Task<Releaser> m_releaser;
+        private readonly SemaphoreSlim m_semaphore;
+        private readonly Task<IDisposable> m_releaser;
 
         public AsyncLock()
         {
             m_semaphore = new SemaphoreSlim(1);
-            m_releaser = Task.FromResult(new Releaser(this));
+            m_releaser = Task.FromResult<IDisposable>(new Releaser(this));
         }
 
-        public struct Releaser : IDisposable
+        private readonly struct Releaser : IDisposable
         {
             readonly AsyncLock m_toRelease;
 
-            internal Releaser(AsyncLock toRelease) { m_toRelease = toRelease; }
+            internal Releaser(AsyncLock toRelease)
+            {
+                m_toRelease = toRelease;
+            }
 
             public void Dispose()
             {
-                if (m_toRelease != null)
-                    m_toRelease.m_semaphore.Release();
+                m_toRelease?.m_semaphore.Release();
             }
         }
 
 #if DEBUG
-        public Task<Releaser> LockAsync([CallerMemberName] string callingMethod = null, [CallerFilePath] string path = null, [CallerLineNumber] int line = 0)
+        public Task<IDisposable> LockAsync([CallerMemberName] string callingMethod = null, [CallerFilePath] string path = null, [CallerLineNumber] int line = 0)
         {
             Debug.WriteLine("AsyncLock.LockAsync called by: " + callingMethod + " in file: " + path + " : " + line);
 #else
-        public Task<Releaser> LockAsync()
+        public Task<IDisposable> LockAsync()
         {
 #endif
             var wait = m_semaphore.WaitAsync();
 
             return wait.IsCompleted ?
                 m_releaser :
-                wait.ContinueWith((_, state) => new Releaser((AsyncLock)state),
+                wait.ContinueWith<IDisposable>((_, state) => new Releaser((AsyncLock)state),
                     this, CancellationToken.None,
                     TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
         }
